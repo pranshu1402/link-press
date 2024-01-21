07ad6c128665e4872f1adc19ec657631d3a76335
pranshu.goyal <pranshu.goyal@deskera.club>
2024-01-21 10:22:16 +0530

    Switching to single db connection

--- src/util/CreateConnection.ts
+++ src/util/CreateConnection.ts
@@ -1,29 +1,29 @@
-import mongoose from 'mongoose';
-import logger from 'jet-logger';
-import { isEmpty } from '@src/util/Functions';
-import EnvVars from '@src/constants/EnvVars';
+import mongoose from "mongoose";
+import logger from "jet-logger";
+import { isEmpty } from "@src/util/Functions";
+import EnvVars from "@src/constants/EnvVars";
 
 const createDBConnection = async () => {
   const connectionString = EnvVars.DbConnectionString;
 
   if (isEmpty(connectionString)) {
-    return Promise.reject('Please provide db url to connect to');
+    return Promise.reject("Please provide db url to connect to");
   }
 
-  mongoose.connection.on('connected', () => {
-    logger.info('Connected to database & bucket created');
+  mongoose.connection.on("connected", () => {
+    logger.info("Connected to database & bucket created");
   });
 
-  mongoose.connection.on('error', err => {
-    logger.info('Error: Connecting to database failed,' + err);
+  mongoose.connection.on("error", (err) => {
+    logger.info("Error: Connecting to database failed," + err);
   });
 
   const connection = await mongoose.connect(connectionString, {
     maxPoolSize: 10,
-    replicaSet: 'rs',
+    // replicaSet: 'rs',
     retryWrites: true,
-    readPreference: 'secondaryPreferred',
-    writeConcern: { w: 'majority', j: true },
+    // readPreference: 'secondaryPreferred',
+    // writeConcern: { w: 'majority', j: true },
   });
 
   return connection;
